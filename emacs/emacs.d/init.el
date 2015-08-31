958164f410ff90bfad12d345bdfd2d50d389db56
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2015-08-30 23:31:49 -0500

    Minimal package management

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -8,8 +8,34 @@
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
 
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+
+Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
+
+;; Make sure to have downloaded archive description.
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+
 (setq package-enable-at-startup nil)
+
+;; Activate installed packages
 (package-initialize)
+
+(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
+
+(ensure-package-installed 'evil 'magit 'sublime-themes)
 
 (require 'evil)
 (evil-mode t)
+
+(load-theme 'brin t)
