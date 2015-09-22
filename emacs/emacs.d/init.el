7b6d162b9e33b69489b62e0dad0931039282325d
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2015-09-22 13:28:33 -0500

    Customize emacs.d
    - Remove marmalade
    - Start customizing packages using use-package

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -3,30 +3,10 @@
 
 (require 'package)
 
-(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
-
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
-
-(defun ensure-package-installed (&rest packages)
-  "Assure every package is installed, ask for installation if it’s not.
-
-Return a list of installed packages or nil for every skipped package."
-  (mapcar
-   (lambda (package)
-     (if (package-installed-p package)
-         nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-           (package-install package)
-         package)))
-   packages))
-
-;; Make sure to have downloaded archive description.
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
 
 (setq package-enable-at-startup nil)
 
@@ -35,9 +15,70 @@ Return a list of installed packages or nil for every skipped package."
 
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
 
-(ensure-package-installed 'evil 'magit 'sublime-themes)
+;; Some defaults
+(setq inhibit-splash-screen t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(if (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
 
-(require 'evil)
-(evil-mode t)
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+;; evil-mode
+(use-package evil
+  :ensure t
+  :init
+  (progn
+    (evil-mode t)
+    (use-package evil-leader
+      :ensure t
+      :init (global-evil-leader-mode)
+      :config
+      (progn
+	(evil-leader/set-leader "SPC")
+	(evil-leader/set-key "wd" 'delete-window)
+	(evil-leader/set-key "wo" 'delete-other-windows)
+	(evil-leader/set-key "ws" 'split-window-below)
+	(evil-leader/set-key "wh" 'split-window-horizontally)
+	(evil-leader/set-key "wv" 'split-window-vertically)
+	(evil-leader/set-key "ww" 'other-window)))
+    (use-package evil-org
+      :ensure t
+      :init (add-hook 'org-mode-hook 'evil-org-mode)
+      :diminish evil-org-mode)))
+
+;; magit
+(use-package magit :ensure t)
+
+;; which-key-mode
+(use-package which-key
+  :ensure t
+  :config
+  (progn
+    (which-key-mode t)
+    (which-key-setup-side-window-right-bottom)))
+
+;; org-mode
+(use-package org :ensure t :pin org)
 
 (load-theme 'atom-one-dark t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("9397f3ba34bdae8fec1183fc0b8aaf09599fab40af69ba58e167e3d2242327c0" default)))
+ '(org-agenda-files (quote ("~/Dropbox/Org/fall2015.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#282C34" :foreground "#ABB2BF" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco")))))
