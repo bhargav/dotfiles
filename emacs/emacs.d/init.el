1085f821d7d0857379de8b8f6370e8c270506889
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2019-07-07 05:28:10 -0700

    [emacs] update config

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,143 +1,84 @@
-(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
-  (normal-top-level-add-subdirs-to-load-path))
+;; Package setup
 
 (require 'package)
 
-;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
-
 (setq package-enable-at-startup nil)
+(setq package-archives '(("org" . "https://orgmode.org/elpa/")
+			 ("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
-;; Activate installed packages
 (package-initialize)
 
-(add-to-list 'exec-path "/usr/local/bin")
-(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
+;; Basic customization
 
-;; Essential settings
-(setq inhibit-splash-screen t
-      inhibit-startup-message t
-      inhibit-startup-echo-area-message t)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
+
 (menu-bar-mode -1)
-;; (tool-bar-mode -1)
-(show-paren-mode t)
-(if (fboundp 'scroll-bar-mode)
-    (scroll-bar-mode -1))
+(show-paren-mode 1)
+(global-linum-mode t)
 
-;; Workaround for El-Capitan
-(setq visible-bell nil)
-(setq ring-bell-function 'ignore)
+(require 'ido)
+(ido-mode t)
 
-;; Bootstrap use-package
-(unless (package-installed-p 'use-package)
+;; Install Packages
+
+(unless(package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)
-;;(setq use-package-verbose t)
 
-;; ido-mde
-(require 'ido)
-(ido-mode t)
+(use-package auto-package-update
+  :ensure t
+  :commands (auto-package-update-now)
+  :config
+  (setq auto-package-update-prompt-before-update t)
+  (setq auto-package-update-delete-old-versions t))
 
-;; evil-mode
+(use-package better-defaults
+  :ensure t)
+
 (use-package evil
   :ensure t
-  :init
-  (progn
-    (evil-mode t)
-    (use-package evil-leader
-      :ensure t
-      :init (global-evil-leader-mode)
-      :config
-      (progn
-	(evil-leader/set-leader "SPC")
-	(evil-leader/set-key "wd" 'delete-window)
-	(evil-leader/set-key "wo" 'delete-other-windows)
-	(evil-leader/set-key "ws" 'split-window-below)
-	(evil-leader/set-key "wh" 'split-window-horizontally)
-	(evil-leader/set-key "wv" 'split-window-vertically)
-	(evil-leader/set-key "ww" 'other-window)))
-    (use-package evil-org
-      :ensure t
-      :init (add-hook 'org-mode-hook 'evil-org-mode)
-      :diminish evil-org-mode)))
+  :config
+  (evil-mode 1))
 
-;; magit
-(use-package magit :ensure t)
+(use-package material-theme
+  :ensure t
+  :config
+  (load-theme 'material t))
 
-;; which-key-mode
+(use-package org
+  :ensure t
+  :pin org)
+
+(use-package powerline
+  :ensure t
+  :config
+  (powerline-center-evil-theme))
+
 (use-package which-key
-  :ensure t
-  :config
-  (progn
-    (which-key-mode t)
-    (which-key-setup-side-window-right-bottom)))
-
-;; org-mode
-(use-package org :ensure t :pin org)
-
-;; helm
-(use-package helm
-  :ensure t
-  :diminish helm-mode
-  :commands helm-mode
-  :config
-  (helm-mode 1))
-
-;; helm-projectile
-(use-package helm-projectile
-  :commands (helm-projectile helm-projectile-switch-project)
-  :ensure t)
-
-;; projectile
-(use-package projectile
-  :ensure t
-  :defer 1
-  :config
-  (projectile-global-mode)
-  (setq projectile-enable-caching t))
-
-;; python-mode
-(use-package python-mode
-  :ensure t)
-
-;; ENSIME
-(use-package ensime
-  :ensure t
-  :pin melpa-stable)
-
-;; scala-mode
-(use-package scala-mode
-  :interpreter
-  ("scala" . scala-mode)
-  :config
-  (setq scala-indent:use-javadoc-style t))
-
-(use-package sbt-mode
-  :commands sbt-start sbt-command
-  :config
-  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
-  ;; allows using SPACE when in the minibuffer
-  (substitute-key-definition
-   'minibuffer-complete-word
-   'self-insert-command
-   minibuffer-local-completion-map))
-
-(load-theme 'misterioso t)
-
+	      :ensure t
+	      :init
+	      (which-key-mode)
+	      :config (which-key-setup-side-window-right-bottom)
+	      (setq which-key-sort-order 'which-key-key-order-alpha
+		    which-key-side-window-max-width 0.33
+		    which-key-idle-delay 0.05))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
  '(package-selected-packages
    (quote
-    (python-mode helm which-key magit evil-org evil-leader evil use-package))))
+    (which-key material-theme evil better-defaults use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
