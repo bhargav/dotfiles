c1a00378d2a2245d7811c5fb81619d9e6a877405
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2019-10-23 18:00:48 -0700

    Migrate i3, fish and yay config

---
+++ config/fish/config.fish
@@ -0,0 +1,15 @@
+set fish_greeting '' # turn off greeting
+
+# Alias
+alias ag="rg -i"
+
+# Configuration for Fisher packages
+set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+set -g fisher_path $XDG_CONFIG_HOME/fisher_home
+
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
+end
